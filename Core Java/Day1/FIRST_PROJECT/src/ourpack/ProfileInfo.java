615c9f5d8a791defd4964f8e22dace4c096a809c
VijayKrishna28 <54760168+VijayKrishna28@users.noreply.github.com>
2019-09-13 22:50:52 +0530

    Add files via upload

---
+++ Core Java/Day1/FIRST_PROJECT/src/ourpack/ProfileInfo.java
@@ -0,0 +1,29 @@
+package ourpack;
+
+public class ProfileInfo {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Name : Vijay Krishna Sharma");
+		
+
+		System.out.println("Qualification : MTech (Computers) ");
+		
+
+		System.out.println("Company : Knowledge Works ");
+		
+
+		System.out.println("Designation : Senior Technology Consultant ");
+		
+
+		System.out.println("City : Bangalore ");
+		
+
+		System.out.println("--------------------------------------------------------------------------------------------- ");
+		
+
+
+	}
+
+}
