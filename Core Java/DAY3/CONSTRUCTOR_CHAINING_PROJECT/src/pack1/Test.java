1741e30f194a5f7f26284b567d759c7d2bade7f3
VijayKrishna28 <54760168+VijayKrishna28@users.noreply.github.com>
2019-09-13 18:19:25 +0530

    Add files via upload

---
+++ Core Java/DAY3/CONSTRUCTOR_CHAINING_PROJECT/src/pack1/Test.java
@@ -0,0 +1,50 @@
+package pack1;
+
+
+
+class Employee
+{
+	Employee()
+	{   super(); //java.lang.Object's constructor
+		System.out.println("Inside Employee Constructor ..............");
+	}
+	
+}
+
+class Manager extends Employee
+{
+	Manager()
+	{
+		super();
+		System.out.println("Inside Manager Constructor ...............");
+	}
+}
+
+class Vice_President extends Manager
+{
+	Vice_President()
+	{
+		super();
+		System.out.println("Inside Vice_President Constructor ..........." );
+	}
+}
+class CEO extends Vice_President
+{
+	CEO()
+	{
+		super();
+		System.out.println("Inside CEO Constructor ............");
+	}
+}
+
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		CEO myceo=new CEO();
+	}
+
+}
