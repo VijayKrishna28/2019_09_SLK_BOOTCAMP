170627c95870f9e1921f3254635cc43c409dd00f
VijayKrishna28 <54760168+VijayKrishna28@users.noreply.github.com>
2019-09-13 23:16:58 +0530

    Add files via upload

---
+++ Core Java/Day1/FULLNAME_PROJECT/src/Concatenator.java
@@ -0,0 +1,27 @@
+
+public class Concatenator {
+
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<3;i++)
+		{
+			System.out.print(args[i]+" ");
+		}
+
+	/*	for(String myval : args)
+		{
+			System.out.print(myval+" ");
+		}
+*/
+		/*String fname=args[0];
+		String mname=args[1];
+		String lname=args[2];
+		
+		String fullname=(fname+"::"+mname+"::"+lname);*/
+		
+		//System.out.println("My name is "+fullname);
+		
+	}
+
+}
