a9c73139ead589a4cbc75c018bfe8b121998b00e
VijayKrishna28 <54760168+VijayKrishna28@users.noreply.github.com>
2019-09-14 10:30:42 +0530

    Add files via upload

---
+++ Core Java/Day1/METHOD_PROJECT/src/teabreak/NumberManipulate.java
@@ -0,0 +1,26 @@
+package teabreak;
+
+public class NumberManipulate {
+
+	
+	
+	
+	static int splitter(int num)
+	{
+		int res=num%100;
+		
+		return res;
+	}
+	
+	
+
+public static void main(String[] args) {
+
+//NumberManipulate obj=new NumberManipulate();
+
+int val=NumberManipulate.splitter(2543);
+
+System.out.println(val);
+	}
+
+}
